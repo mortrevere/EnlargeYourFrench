2b8a22885290143fbd5d3803c4df41aa05dced10
mortrevere <leodroide@hotmail.fr>
2020-05-03 10:28:42 +0200

    change point counting

--- main.py
+++ main.py
@@ -52,6 +52,7 @@ class Game:
         self.definition = None
         self.kill_switch = False
         self.scores = {}
+        self.current_hint = ""
 
     async def start(self):
         await self.channel.send(
@@ -73,28 +74,24 @@ class Game:
             self.word_start_time = time.time()
             self.word, self.definition = wikidict.get_word_and_definition()
             current_word = self.word
-            current_hint = "".join(
+            self.current_hint = "".join(
                 ["_" if l in string.ascii_lowercase else l for l in current_word]
             )
             # TODO hint avec les espaces / - déjà ?
             await self.channel.send(
                 "{} lettres : {}".format(len(self.word), self.definition)
             )
-            max_hints = len(current_word) - math.ceil(
+            max_hints = len(current_word) - (math.ceil(
                 (1 - TOTAL_HINT_PERCENT) * len(current_word)
-            ) * math.floor(len(current_word) * PERCENT_PER_HINT)
-            print(
-                "hidden letters : ",
-                math.ceil((1 - TOTAL_HINT_PERCENT) * len(current_word))
-                * math.floor(len(current_word) * PERCENT_PER_HINT),
-            )
+            ) * math.floor(len(current_word) * PERCENT_PER_HINT))
+
             for i in range(max_hints):
                 await asyncio.sleep(TIME_PER_HINT)
                 if current_word != self.word:
                     return
                 if i < max_hints:
-                    current_hint = add_hint(current_hint, current_word)
-                    await self.channel.send(f"**Indice** : `{current_hint}`")
+                    self.current_hint = add_hint(self.current_hint, current_word)
+                    await self.channel.send(f"**Indice** : `{self.current_hint}`")
 
             time_elapsed_for_word = time.time() - self.word_start_time
             if time_elapsed_for_word < WORD_TIME_LIMIT:
@@ -113,9 +110,9 @@ class Game:
         current_word = self.word
         self.word = None
         if player_id in self.scores:
-            self.scores[player_id] += len(current_word)
+            self.scores[player_id] += self.current_hint.count('_')
         else:
-            self.scores[player_id] = len(current_word)
+            self.scores[player_id] = self.current_hint.count('_')
 
         if self.scores[player_id] >= POINTS_LIMIT:
             await self.channel.send(
