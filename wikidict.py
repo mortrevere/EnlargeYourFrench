ac3510f7abf13fb55df82720196975aec1b78b30
klemek <klemek@outlook.fr>
2020-05-03 15:08:21 +0200

    using API to get full list

--- wikidict.py
+++ wikidict.py
@@ -3,54 +3,72 @@ import re
 import html
 import requests
 import random
+import os
 
 VOWELS = "aeiouy"
 CONSONANTS = "bcdfghjklmnpqrstvwz"
-WIKI_ENDPOINT = "https://en.wiktionary.org/w/index.php?title=Category%3AFrench_lemmas&printable=yes&pagefrom={}"
+WIKI_ENDPOINT_EN = "https://en.wiktionary.org/w/api.php"
+WIKI_ENDPOINT_FR = "https://fr.wiktionary.org/w/index.php"
+
+LIST_FILE = "wikidict.txt"
 
 
-def random_tag() -> str:
-    if random.random() > 0.88:
-        return random.choice(VOWELS)
-    else:
-        return random.choice(CONSONANTS) + random.choice(VOWELS)
+def create_list_file():
+    print("loading from API...")
+    session = requests.Session()
+    params = {
+        "format": "json",
+        "action": "query",
+        "list": "categorymembers",
+        "cmtitle": "Category:French_lemmas",
+        "cmlimit": "500"
+    }
+    response = None
+    data = None
+    cont_key = None
+    start = True
+    count = 0
+    with open(LIST_FILE, mode="w", encoding="utf8") as f:
+        while start or cont_key is not None:
+            start = False
+            if cont_key is not None:
+                params["cmcontinue"] = cont_key
+            response = session.get(url=WIKI_ENDPOINT_EN, params=params)
+            data = response.json()
+            cont_key = None
+            if "continue" in data and "cmcontinue" in data["continue"]:
+                cont_key = data["continue"]["cmcontinue"]
+            if "query" in data and "categorymembers" in data["query"]:
+                for word in data["query"]["categorymembers"]:
+                    word = word['title']
+                    if not (
+                            len(word) < 3 or
+                            word[0].isupper() or
+                            re.match(r"\w", word[0]) is None or
+                            re.match(r"\d", word[0]) is not None or
+                            re.match(r"\w", word[-1]) is None or
+                            word[-4:] == "ment" or
+                            word.count(" ") > 2
+                    ):
+                        f.write(word + "\n")
+                        count += 1
+            print(count)
 
 
-def simplify(words: List[str]) -> List[str]:
-    rm_candidates = []
-    for word in words:
-        conditions_for_rm = [
-            len(word) < 3,
-            word.isupper(),
-            word[0].isupper(),
-            word[0] == "-",
-            word[-1] == "-",
-            word[-4:] == "ment",
-            word.count(" ") > 2,
-        ]
-        if True in conditions_for_rm:
-            rm_candidates += [word]
-    return [word for word in words if word not in rm_candidates]
+def load_list() -> List[str]:
+    if not os.path.exists(LIST_FILE):
+        create_list_file()
+    with open(LIST_FILE, mode="r", encoding="utf8") as f:
+        return [word.strip() for word in f.readlines() if len(word.strip()) > 0]
 
 
-def get_random_word() -> Tuple[str, str]:
-    r = requests.get(WIKI_ENDPOINT.format(random_tag()))
+print("loading wiktionary words...")
+WORDS = load_list()
+print(f"loaded {len(WORDS)} words")
 
-    words = {}
-    start_after = '<li><a href="/wiki/Category:French_verbs"'
-    found = False
 
-    for line in r.content.decode().split("\n"):
-        if found and line.startswith('<li><a href="/wiki/'):
-            tmp = line[19 : line.find("</a>")]
-            word = tmp[tmp.find(">") + 1 :]
-            URL = tmp[0 : tmp.find('"')]
-            words[word] = URL
-        if line.startswith(start_after):
-            found = True
-
-    word = random.choice(simplify(list(words.keys())))
-    return word, words[word]
+def get_random_word() -> str:
+    return random.choice(WORDS)
 
 
 def remove_html(raw_html: str) -> str:
@@ -58,10 +76,12 @@ def remove_html(raw_html: str) -> str:
     return re.sub(cleanr, "", raw_html)
 
 
-def get_definition(url) -> Optional[str]:
-    r = requests.get(
-        "https://fr.wiktionary.org/w/index.php?title={}&printable=yes".format(url)
-    )
+def get_definition(word) -> Optional[str]:
+    params = {
+        "title": word,
+        "printable": "yes"
+    }
+    r = requests.get(url=WIKI_ENDPOINT_FR, params=params)
     # print(r.content.decode())
     result = re.search("<ol>(.*)</ol>", r.content.decode().replace("\n", ""))
     if not result:
@@ -86,8 +106,8 @@ def get_word_and_definition() -> Tuple[str, str]:
     word, url = None, None
     while definition is None:
         success = True
-        word, url = get_random_word()
-        definition = get_definition(url)
+        word = get_random_word()
+        definition = get_definition(word)
         if definition is not None:
             if definition.find("Variante") != -1 or len(definition.split(" ")) < 5:
                 definition = None
